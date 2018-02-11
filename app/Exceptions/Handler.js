b9b36adc38538b354be0d200a618f5ce7237bd14
keeross <kee.ross@dolsen.co>
2018-02-12 00:00:34 +0200

    Updated Exceptions Handler

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,8 +1,8 @@
 'use strict'
 
 class Handler {
-  
-  _getYouchError (error, req, isJSON) {
+
+  _getYouchError(error, req, isJSON) {
     const Youch = require('youch')
     const youch = new Youch(error, req)
     if (isJSON) {
@@ -10,8 +10,8 @@ class Handler {
     }
     return youch.toHTML()
   }
-  
-  _getPlainError (error, isJSON) {
+
+  _getPlainError(error, isJSON) {
     return isJSON ? {
       message: error.message,
       name: error.name,
@@ -19,18 +19,33 @@ class Handler {
       status: error.status
     } : `${error.name}: ${error.message}`
   }
-  
-  async handle (error, { request, response, view }) {
-    const isJSON = request.accepts(['html', 'json']) === 'json'    
 
+  async handle(error, { request, response, session, view }) {
+    const isJSON = request.accepts(['html', 'json']) === 'json'
+
+    // validation exception
+    if (error.name === 'ValidationException') {
+      session.withErrors(error.messages).flashAll()
+      await session.commit()
+      response.redirect('back')
+      return
+    }
+
+    // development exception
     if (process.env.NODE_ENV === 'development') {
       const formattedError = await this._getYouchError(error, request.request, isJSON)
       response.status(error.status).send(formattedError)
       return
     }
 
-    return response.send( view.render('errors.index', { error }) )
+    return response.send(view.render('errors.index', { error }))
+
   }
+
+  async report(error, { request }) {
+    console.log(error)
+  }
+
 }
 
 module.exports = Handler
